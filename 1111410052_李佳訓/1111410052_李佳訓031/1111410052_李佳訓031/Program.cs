41fa57844ec45e2695d2dab89aeed12d0bb33dba
LiJiaXun52 <134314952+LiJiaXun52@users.noreply.github.com>
2024-04-14 18:27:56 +0800

    Add files via upload

---
+++ 1111410052_李佳訓/1111410052_李佳訓031/1111410052_李佳訓031/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _1111410052_李佳訓031
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words;
+            char x;
+
+            Console.WriteLine("請輸入一個字串陣列（以半形逗號分隔）:");
+            words = Console.ReadLine().Split(',');
+
+            Console.WriteLine("您要尋找的字元:");
+            x = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            List<int> indices = new List<int>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+
+                if (words[i].IndexOf(x) != -1)
+                {
+                    indices.Add(i);
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine("包含字元的單字的索引陣列:");
+            foreach (int index in indices)
+            {
+                Console.Write(index + " ");
+            }
+        }
+    }
+}
